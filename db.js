02955a29411d95f0e41ab71f8c33f2d1ec466e8b
rtio <meneses.tio@gmail.com>
2023-10-09 20:36:41 -0300

    monday

---
+++ db.js
@@ -0,0 +1,54 @@
+const pessoas = [
+    {
+        id: 1,
+        nome: 'João',
+        idade: 30,
+        email: 'joao@joao.net'
+    },
+    {
+        id: 2,
+        nome: 'Maria',
+        idade: 25,
+        email: 'maria@maria.net',
+    },
+    {
+        id: 3,
+        nome: 'José',
+        idade: 40,
+        email: 'jose@jose.net'
+    },
+    {
+        id: 4,
+        nome: 'Ana',
+        idade: 35,
+        email: 'ana@ana.net'
+    }
+];
+
+const carros = [
+    {
+        id: 1,
+        nome: 'Gol',
+        ano: 2010,
+    },
+    {
+        id: 2,
+        nome: 'Palio',
+        ano: 2011,
+    },
+    {
+        id: 3,
+        nome: 'Uno',
+        ano: 2012,
+    },
+    {
+        id: 4,
+        nome: 'Celta',
+        ano: 2013,
+    }
+];
+
+module.exports = {
+    pessoas,
+    carros
+};
--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 const http = require('http');
+const db = require('./db');
+const pessoas = db.pessoas;
+const carros = db.carros;
  
 const hostname = '127.0.0.1';
 const port = 3000;
  
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/json');
-  res.end('Hello World');
+  if (req.method === 'GET') {
+    if (req.url === '/pessoas') {
+      res.statusCode = 200;
+      res.end(JSON.stringify(pessoas));
+    } else if (req.url === '/carros') {
+      res.statusCode = 200;
+      res.end(JSON.stringify(carros));
+    } else if (req.url.match(/\/pessoas\/[0-9]+/)) {
+      const id = req.url.split('/')[2];
+      const pessoa = pessoas.find(p => p.id == id);
+      if (pessoa) {
+        res.statusCode = 200;
+        res.end(JSON.stringify(pessoa));
+      } else {
+        res.statusCode = 404;
+        res.end();
+      }
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
 });
  
 server.listen(port, hostname, () => {
